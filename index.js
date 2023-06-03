9667d959675a4abb2701eaab54d434b32d6fbbe6
Jedynakk <jedynakk.bartlomiej@gmail.com>
2023-06-03 20:04:05 +0200

    Finished calculator

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const numberButton = document.querySelectorAll('.number')
 const mathSignButton = document.querySelectorAll('.mathSign')
 const clearButton = document.querySelector('.clear')
-const equalsButton = document.querySelector('equals')
+const equalsButton = document.querySelector('.equals')
 const currentNumber = document.querySelector('.currentNumber')
 const previousNumber = document.querySelector('.previousNumber')
 const sign = document.querySelector('.sign')
-const dot = document.querySelector('.dot')
 const doubleZero = document.querySelector('.doubleZero')
 
 
 clearButton.addEventListener('click', clear)
 numberButton.forEach((button) => button.addEventListener('click', numbers))
 mathSignButton.forEach((button) => button.addEventListener('click', signs))
-dot.addEventListener('click', addDot)
 doubleZero.addEventListener('click', addDoubleZero)
+equalsButton.addEventListener('click', showResult)
 
 function numbers() {
+    if (this.textContent === "." && currentNumber.innerHTML.includes('.')) return;
     currentNumber.innerHTML += this.innerHTML;
 }
 
 function signs() {
     if (currentNumber.innerHTML != '') {
         sign.innerHTML = this.innerHTML;
+        previousNumber.innerHTML = currentNumber.innerHTML
+        currentNumber.innerHTML = '';
     }
-    previousNumber.innerHTML = currentNumber.innerHTML
-    currentNumber.innerHTML = '';
-
 }
 
 
@@ -35,14 +34,39 @@ function clear() {
     sign.innerHTML = '';
 }
 
-function addDot() {
-    if (currentNumber.innerHTML != '') {
-        currentNumber.innerHTML += this.innerHTML
-    }
-}
 
 function addDoubleZero() {
     if (currentNumber.innerHTML != '') {
         currentNumber.innerHTML += this.innerHTML
     }
+}
+
+
+function showResult() {
+    let a = Number(currentNumber.innerHTML);
+    let b = Number(previousNumber.innerHTML);
+    let mSign = sign.innerHTML;
+    switch (mSign) {
+        case '+':
+            currentNumber.innerHTML = b + a;
+            break;
+        case '-':
+            currentNumber.innerHTML = b - a;
+            break;
+        case ':':
+            currentNumber.innerHTML = b / a;
+            break;
+        case 'x':
+            currentNumber.innerHTML = b * a;
+            break;
+        case '2^':
+            currentNumber.innerHTML = b ** a;
+            break;
+        case '%':
+            currentNumber.innerHTML = (b * 0.01) * a;
+            break;
+
+    }
+    previousNumber.innerHTML = "";
+    sign.innerHTML = "";
 }
